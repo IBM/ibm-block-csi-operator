c673c22028b05385c184296c363d0e93646b5498
igorgon <igorgon@il.ibm.com>
2019-12-12 17:21:03 +0200

    snapshot_operator

--- pkg/controller/ibmblockcsi/syncer/csi_controller.go
+++ pkg/controller/ibmblockcsi/syncer/csi_controller.go
@@ -158,7 +158,7 @@ func (s *csiControllerSyncer) ensureContainersSpec() []corev1.Container {
 	// csi snapshotter sidecar
 	snapshotter := s.ensureContainer(snapshotterContainerName,
 		s.getCSISnapshotterImage(),
-		[]string{"--csi-address=$(ADDRESS)", "--v=5", "--kubeconfig=~/.kube/config"},
+		[]string{"--csi-address=$(ADDRESS)", "--v=5"},
 	)
 	snapshotter.ImagePullPolicy = s.getCSISnapshotterPullPolicy()
 
