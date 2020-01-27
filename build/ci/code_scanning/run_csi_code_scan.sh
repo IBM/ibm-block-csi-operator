53920203b68a65c58f557e697a9346e6a907b6fc
Anna Lisyuk <anna.lisyuk@ibm.com>
2020-01-27 13:13:22 +0200

    Fix for code scanning results

--- build/ci/code_scanning/run_csi_code_scan.sh
+++ build/ci/code_scanning/run_csi_code_scan.sh
@@ -2,10 +2,12 @@
 
 TARGET_NAME=$1
 OUTPUT_PATH="`pwd`/build/reports/${TARGET_NAME}"
-BASELINE_PATH="/root/code_scan/${TARGET_NAME}"
+BASELINE_PATH="`pwd`/build/code_scan/${TARGET_NAME}"
+
+mkdir ${BASELINE_PATH} && cp -r /root/code_scan/${TARGET_NAME}/* ${BASELINE_PATH}
 
 docker build -f build/ci/code_scanning/Dockerfile-${TARGET_NAME} -t ${TARGET_NAME} . && \
 docker run -e TARGET_NAME=${TARGET_NAME} --rm -t -v ${OUTPUT_PATH}:/results ${TARGET_NAME}
 
 docker build -f build/ci/code_scanning/Dockerfile-csi-code-scan-results -t csi-code-scan-results . && \
-docker run -e TARGET_NAME=${TARGET_NAME} --rm -t -v ${OUTPUT_PATH}:/results -v ${BASELINE_PATH}:/baseline_results csi-code-scan-results || exit $?
+docker run -e TARGET_NAME=${TARGET_NAME} --rm -t -v ${OUTPUT_PATH}:/results -v ${BASELINE_PATH}:/baseline_results csi-code-scan-results
