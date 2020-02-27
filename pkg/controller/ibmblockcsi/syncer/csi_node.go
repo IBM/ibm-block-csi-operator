917bc0009733da7b5870f4e4471bf9e2b0475c0b
igorgon <igorgon@il.ibm.com>
2020-02-27 12:23:48 +0200

    CSI-1191_iscsi_login_deployment - try to remove host networking

--- pkg/controller/ibmblockcsi/syncer/csi_node.go
+++ pkg/controller/ibmblockcsi/syncer/csi_node.go
@@ -97,7 +97,7 @@ func (s *csiNodeSyncer) ensurePodSpec() corev1.PodSpec {
 		Containers:         s.ensureContainersSpec(),
 		Volumes:            s.ensureVolumes(),
 		HostIPC:            true,
-		HostNetwork: 		true,
+		//TODO: HostNetwork: 		true,
 		ServiceAccountName: config.GetNameForResource(config.CSINodeServiceAccount, s.driver.Name),
 		Affinity:           s.driver.Spec.Node.Affinity,
 		Tolerations:        s.driver.Spec.Node.Tolerations,
