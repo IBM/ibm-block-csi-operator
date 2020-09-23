39a4c79fa1df88635abc745653f073fb5fbed131
Matan Carmeli <matan.carmeli@ibm.com>
2020-09-23 13:36:50 +0300

    add bash script to set the environment variables

---
+++ publish/set_the_environment_variables.sh
@@ -0,0 +1,11 @@
+for job in $jobs
+do
+	if [ $job =~ "k8s"]; then
+    	echo "hi"
+    	echo x86_k8s_svc_jenkins_job=matan_test > $WORKSPACE/env.propert
+    elif [ $job =~ "production_rhel_svc"]; then
+    	echo x86_ocp_svc_jenkins_job=$job >> $WORKSPACE/env.propert
+    else
+    	echo z_ocp_svc_jenkins_job=$job >> $WORKSPACE/env.propert
+    fi
+don
