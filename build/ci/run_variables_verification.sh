021ff4d404b23d750aa1d904939a5f3f67a1c225
Matan Carmeli <matan.carmeli@ibm.com>
2020-10-20 18:24:22 +0300

    change the mandatory variables

--- build/ci/run_variables_verification.sh
+++ build/ci/run_variables_verification.sh
@@ -1,5 +1,5 @@
 # Validations
-MANDATORY_ENVS="IMAGE_VERSION BUILD_NUMBER DOCKER_REGISTRY NODE_IMAGE CONTROLLER_IMAGE GIT_BRANCH"
+MANDATORY_ENVS="IMAGE_VERSION BUILD_NUMBER DOCKER_REGISTRY OPERATOR_IMAGE GIT_BRANCH"
 for envi in $MANDATORY_ENVS; do 
     [ -z "${!envi}" ] && { echo "Error - Env $envi is mandatory for the script."; exit 1; } || :
 done
