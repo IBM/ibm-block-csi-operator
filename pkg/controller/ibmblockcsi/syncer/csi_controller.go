834c589f6e9a92869e2982f5ea71df3c2fc8f8cd
Arbel Nathan <arbel.nathan@ibm.com>
2021-05-24 14:00:26 +0300

    feature/CSI-2899_topology_controller_support

--- pkg/controller/ibmblockcsi/syncer/csi_controller.go
+++ pkg/controller/ibmblockcsi/syncer/csi_controller.go
@@ -137,7 +137,7 @@ func (s *csiControllerSyncer) ensureContainersSpec() []corev1.Container {
 	provisioner := s.ensureContainer(provisionerContainerName,
 		s.getCSIProvisionerImage(),
 		// TODO: make timeout configurable
-		[]string{"--csi-address=$(ADDRESS)", "--v=5", "--timeout=30s", "--default-fstype=ext4"},
+		[]string{"--csi-address=$(ADDRESS)", "--v=5", "--timeout=30s", "--default-fstype=ext4", "--feature-gates=Topology=true"},
 	)
 	provisioner.ImagePullPolicy = s.getCSIProvisionerPullPolicy()
 
