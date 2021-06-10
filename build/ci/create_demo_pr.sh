b267d6d6ba86fb2608796f43db0cc7d629ab3c6a
matancarmeli7 <matan.carmeli7@gmail.com>
2021-06-10 11:56:10 +0300

    remove sed from create_demo_pr.sh

--- build/ci/create_demo_pr.sh
+++ build/ci/create_demo_pr.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -xe
 set +o pipefail
 
-sed -i "s+$current_operator_image+$wanted_operator_image+g" $csv_file
-
 # install gh command
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
