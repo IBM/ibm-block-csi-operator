f58e6f493eeba993456e6786e97eb3cbb6c08c78
matancarmeli7 <matan.carmeli7@gmail.com>
2021-06-14 09:43:04 +0300

    create are_all_tests_were_passed function

--- build/ci/check_demo_pr_checks.sh
+++ build/ci/check_demo_pr_checks.sh
@@ -6,7 +6,7 @@ export passed_openshift_checks=true
 
 wait_for_checks_to_complete(){
   community_operators_branch=$1
-  all_checks_passed=true
+  is_summary_test_passed=false
   export repo_pr=`gh pr list --repo $github_csiblock_community_operators_repository | grep $community_operators_branch`
   if [[ "$repo_pr" == *"$community_operators_branch"* ]]; then
     sleep 5
@@ -15,29 +15,37 @@ wait_for_checks_to_complete(){
     done
     summary_counter=0
     export search_summary_test=`gh pr checks $community_operators_branch --repo $github_csiblock_community_operators_repository | grep -i summary`
-    while [ "$search_summary_test" != *"pending"* ] && [ $summary_counter -lt 20 ]; do
+    while [ "$search_summary_test" != *"pending"* ] && [ $summary_counter -lt 20 ] && [ $is_summary_test_passed == "false" ]; do
       sleep 1
       export search_summary_test=`gh pr checks $community_operators_branch --repo $github_csiblock_community_operators_repository | grep -i summary`
       ((summary_counter=summary_counter+1))
       if [[ "$search_summary_test" == *"pass"* ]]; then
-        summary_counter=20
+        is_summary_test_passed=true
       fi
     done
-    if [[ $summary_counter -lt 20 ]]; then
+    if [[ $is_summary_test_passed == "false" ]]; then
       while [ "$(gh pr checks $community_operators_branch --repo $github_csiblock_community_operators_repository | grep -i pending | wc -l)" -gt 0 ]; do
       sleep 5
     done
     fi
-    if [ "$(gh pr checks $community_operators_branch --repo $github_csiblock_community_operators_repository | grep -iv pass | wc -l)" -gt 0 ]
-    then
-      export all_checks_passed=false
-    fi
-    echo "$all_checks_passed"
   fi
 }
 
-passed_k8s_checks=$(wait_for_checks_to_complete $community_operators_kubernetes_branch)
-passed_openshift_checks=$(wait_for_checks_to_complete $community_operators_openshift_branch)
+are_all_tests_were_passed(){
+  community_operators_branch=$1
+  all_checks_passed=false
+  if [ "$(gh pr checks $community_operators_branch --repo $github_csiblock_community_operators_repository | grep -iv pass | wc -l)" -eq 0 ]
+  then
+    export all_checks_passed=true
+  fi
+  echo "$all_checks_passed"
+}
+
+wait_for_checks_to_complete $community_operators_kubernetes_branch
+wait_for_checks_to_complete $community_operators_openshift_branch
+
+passed_k8s_checks=$(are_all_tests_were_passed $community_operators_kubernetes_branch)
+passed_openshift_checks=$(are_all_tests_were_passed $community_operators_openshift_branch)
 
 if [ $passed_k8s_checks == "false" ] || [ $passed_openshift_checks == "false" ]
 then
