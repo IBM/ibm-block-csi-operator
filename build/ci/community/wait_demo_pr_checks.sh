5eff30ae6f4850e69a5ffa9d2145dd6bd9674df7
matancarmeli7 <matan.carmeli7@gmail.com>
2021-06-16 10:14:44 +0300

    call gh_pr_checks_command as variable

--- build/ci/community/wait_demo_pr_checks.sh
+++ build/ci/community/wait_demo_pr_checks.sh
@@ -8,21 +8,21 @@ wait_for_checks_to_complete(){
   export repo_pr=`gh pr list --repo $github_csiblock_community_operators_repository | grep $community_operators_branch`
   if [[ "$repo_pr" == *"$community_operators_branch"* ]]; then
     sleep 5
-    while [ `eval gh_pr_checks_command | grep -i pending | wc -l` -gt 0 ]; do
+    while [ `eval $gh_pr_checks_command | grep -i pending | wc -l` -gt 0 ]; do
       sleep 15
     done
     seconds_waited_for_summary_test=0
     seconds_to_wait_for_summary_test=20
     while [ "$test_summary" != *"pending"* ] && [ $seconds_waited_for_summary_test -lt $summary_test_timeout_seconds] && [ $all_tests_passed == "false" ]; do
       sleep 1
-      test_summary=`eval gh_pr_checks_command | grep -i summary`
+      test_summary=`eval $gh_pr_checks_command | grep -i summary`
       ((seconds_waited_for_summary_test=seconds_waited_for_summary_test+1))
       if [[ "$test_summary" == *"pass"* ]]; then
         all_tests_passed=true
       fi
     done
     if [[ $all_tests_passed == "false" ]]; then
-      while [ `eval gh_pr_checks_command | grep -i pending | wc -l` -gt 0 ]; do
+      while [ `eval $gh_pr_checks_command | grep -i pending | wc -l` -gt 0 ]; do
         sleep 5
       done
     fi
