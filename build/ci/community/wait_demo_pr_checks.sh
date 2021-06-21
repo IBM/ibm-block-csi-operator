c30c38162b4c21885891118cb8b51c4f4b546691
matancarmeli7 <matan.carmeli7@gmail.com>
2021-06-21 12:03:38 +0300

    add parameters to wait_fot_checks_to_start

--- build/ci/community/wait_demo_pr_checks.sh
+++ build/ci/community/wait_demo_pr_checks.sh
@@ -2,7 +2,9 @@
 set +o pipefail
 
 wait_fot_checks_to_start(){
-  gh_pr_checks_command=$1
+  community_operators_branch=$1
+  forked_community_operators_repository=$2
+  gh_pr_checks_command="gh pr checks $community_operators_branch --repo $forked_community_operators_repository"
   while [ `$gh_pr_checks_command | grep -i pending | wc -l` -eq 0 ]; do
     sleep 1
   done
@@ -13,7 +15,7 @@ wait_for_checks_to_complete(){
   gh_pr_checks_command="gh pr checks $community_operators_branch --repo $forked_community_operators_repository"
   repo_pr=`gh pr list --repo $forked_community_operators_repository | grep $community_operators_branch`
   if [[ "$repo_pr" == *"$community_operators_branch"* ]]; then
-    wait_fot_checks_to_start $gh_pr_checks_command
+    wait_fot_checks_to_start $community_operators_branch $forked_community_operators_repository
     test_summary="$gh_pr_checks_command | grep -i summary"
     while [[ ! "$test_summary" =~ "pass" ]] && [[ ! "$test_summary" =~ "fail" ]]; do
       sleep 1
