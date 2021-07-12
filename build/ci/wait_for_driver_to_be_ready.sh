359459bf6f49b1e0b2de3ceaa582bb41db6686c9
matancarmeli7 <matan.carmeli7@gmail.com>
2021-07-12 12:15:58 +0300

    answer some code review comments

--- build/ci/wait_for_driver_to_be_ready.sh
+++ build/ci/wait_for_driver_to_be_ready.sh
@@ -2,15 +2,15 @@
 set +o pipefail
 
 driver_is_ready=false
-amount_of_seconds_that_driver_pods_are_running=0
-amount_of_seconds_that_driver_pods_should_be_running=10
+actual_driver_running_time_in_seconds=0
+minimum_driver_running_time_in_seconds=10
 while [ "$(kubectl get pod -A -l csi | grep controller | wc -l)" -eq 0 ]; do
   echo "The controller is not deployed"
   sleep 1
 done
 while [ $driver_is_ready == "false" ]; do
   if [ "$(kubectl get pod -A -l csi | grep -iv running | grep -iv name | wc -l)" -eq 0 ]; then
-    ((amount_of_seconds_that_driver_pods_are_running=amount_of_seconds_that_driver_pods_are_running+1))
+    ((amount_of_seconds_that_driver_pods_are_running++))
     if [ $amount_of_seconds_that_driver_pods_are_running -eq $amount_of_seconds_that_driver_pods_should_be_running ]; then
       driver_is_ready=true
     fi
