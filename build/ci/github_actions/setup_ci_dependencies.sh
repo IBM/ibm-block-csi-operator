aba7bb060c67f23e38e5089145730a6a5b3e84ee
matancarmeli7 <matan.carmeli7@gmail.com>
2021-10-04 09:38:00 +0300

    add setup_yq.sh
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- build/ci/github_actions/setup_ci_dependencies.sh
+++ build/ci/github_actions/setup_ci_dependencies.sh
@@ -2,18 +2,13 @@
 set +o pipefail
 
 install_ci_dependencies (){
+  build/ci/github_actions/setup_yq.sh
   python -m pip install --upgrade pip==21.2.4
   echo docker-hub==2.2.0 > dev-requirements.txt
   pip install -r dev-requirements.txt
 }
 
 install_ci_dependencies
-cat >>/home/runner/.bash_profile <<'EOL'
-yq() {
-  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
-}
-EOL
-
 # CSI-3173 - move image_version value into a common config file
 image_version=`cat version/version.go | grep -i driverversion | awk -F = '{print $2}'`
 image_version=`echo ${image_version//\"}`
