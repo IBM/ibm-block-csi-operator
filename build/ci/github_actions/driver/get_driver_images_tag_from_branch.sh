3642121ada2fb18244317c3f304ad2e98c6c554a
matancarmeli7 <matan.carmeli7@gmail.com>
2021-10-17 10:14:15 +0300

    edit get_driver_image script
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- build/ci/github_actions/driver/get_driver_images_tag_from_branch.sh
+++ build/ci/github_actions/driver/get_driver_images_tag_from_branch.sh
@@ -3,23 +3,20 @@ set +o pipefail
 
 driver_images_tag_from_branch=latest
 triggering_branch=$CI_ACTION_REF_NAME
-target_image_tags=`build/ci/get_image_tags_from_branch.sh ${triggering_branch}`
-target_specific_tag=`echo $target_image_tags | awk '{print$2}'`
+branch_image_tag=`build/ci/get_image_tags_from_branch.sh ${triggering_branch}`
+target_specific_tag=`echo $branch_image_tag | awk '{print$2}'`
 
 is_private_branch_component_image_exists(){
   driver_component=$1
+  image_to_check=$csiblock_docker_registry_username/ibm-block-csi-$driver_component:$target_specific_tag
   is_image_tag_exists=false
-  export driver_image_inspect=`docker manifest inspect $csiblock_docker_registry_username/ibm-block-csi-$driver_component:$target_specific_tag`
-  if [[ "$driver_image_inspect" != "" ]]; then
-    is_image_tag_exists=true
-  fi
-  echo $is_image_tag_exists
+  export driver_image_inspect=`docker manifest inspect $image_to_check &> /dev/null; echo $?`
 }
 
 is_controller_image_tag_exists=$(is_private_branch_component_image_exists controller)
 is_node_image_tag_exists=$(is_private_branch_component_image_exists node)
 
-if [ $is_controller_image_tag_exists == "true" ] && [ $is_node_image_tag_exists == "true" ]; then
+if [ $is_controller_image_tag_exists == "0" ] && [ $is_node_image_tag_exists == "0" ]; then
   driver_images_tag_from_branch=$target_specific_tag
 fi
 
