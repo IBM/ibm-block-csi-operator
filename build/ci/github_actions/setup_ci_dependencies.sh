3ff23d661f6cf588c3debffd5445cb41a1a866c1
matancarmeli7 <matan.carmeli7@gmail.com>
2021-11-02 09:39:20 +0200

    align setup_ci with driver
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- build/ci/github_actions/setup_ci_dependencies.sh
+++ build/ci/github_actions/setup_ci_dependencies.sh
@@ -9,16 +9,18 @@ install_ci_dependencies (){
 }
 
 install_ci_dependencies
+triggering_branch = ${CI_ACTION_REF_NAME}
 # CSI-3173 - move image_version value into a common config file
 image_version=$(cat version/version.go | grep -i driverversion | awk -F = '{print $2}')
 image_version=$(echo ${image_version//\"})
-operator_image_tags_for_test=$(build/ci/get_image_tags_from_branch.sh ${CI_ACTION_REF_NAME} ${image_version} ${build_number} ${GITHUB_SHA})
-branch_image_tag=$(echo $operator_image_tags_for_test | awk '{print$2}')
+operator_image_tags_for_test=$(build/ci/get_image_tags_from_branch.sh ${triggering_branch} ${image_version} ${build_number} ${GITHUB_SHA})
 operator_specific_tag_for_test=$(echo $operator_image_tags_for_test | awk '{print$1}')
 
-if [ "$branch_image_tag" == "develop" ]; then
+if [ "$triggering_branch" == "develop" ]; then
   branch_image_tag=latest
+else
+  branch_image_tag=$(echo $operator_image_tags_for_test | awk '{print$2}')
 fi
 
-echo "::set-output name=branch_image_tag::${branch_image_tag}"
 echo "::set-output name=operator_specific_tag_for_test::${operator_specific_tag_for_test}"
+echo "::set-output name=branch_image_tag::${branch_image_tag}"
