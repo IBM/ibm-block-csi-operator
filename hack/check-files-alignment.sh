b5c03d17a41d7f982e2cdd5a52ff1098e940cd20
matancarmeli7 <matan.carmeli7@gmail.com>
2021-11-25 14:37:04 +0200

    check all yamls
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- hack/check-files-alignment.sh
+++ hack/check-files-alignment.sh
@@ -21,6 +21,7 @@ roles_yaml_path=config/rbac/role.yaml
 origin_crd_yaml_path=config/crd/bases/csi.ibm.com_ibmblockcsis.yaml
 
 check_generation (){
+  echo "check generation"
   project_dirname=ibm-block-csi-operator
   cd ..
   cp -r $project_dirname ./$project_dirname-expected
@@ -32,25 +33,42 @@ check_generation (){
   cd $project_dirname
 }
 
+verify_full_operator_yaml_is_align(){
+  echo "check full operator yaml alignment"
+  declare -A orignial_yamls=(
+      ["config/rbac/role.yaml"]="ClusterRole"
+      ["config/crd/bases/csi.ibm.com_ibmblockcsis.yaml"]="CustomResourceDefinition"
+      ["config/rbac/service_account.yaml"]="ServiceAccount"
+      ["config/rbac/role_binding.yaml"]="ClusterRoleBinding"
+      ["config/manager/manager.yaml"]="Deployment"
+  )
+  for orignial_yaml in ${!orignial_yamls[@]}; do
+    export resource_type=${orignial_yamls[${orignial_yaml}]}
+    diff <(yq e '... comments=""' $orignial_yaml) <(yq eval '(. | select(.kind == env(resource_type)))' $operator_yaml_path)
+  done
+}
+
 verify_no_roles_diff (){
+  echo "check roles alignment"
   source hack/get_information_helper.sh
   are_manifest_files_exsists_in_current_csi_version
   csv_files=$(get_csv_files)
   for csv_file in $csv_files; do
     diff <(yq e .rules $roles_yaml_path) <(yq e .spec.install.spec.clusterPermissions[0].rules $csv_file)
   done
-  diff <(cat $roles_yaml_path) <(yq eval '(. | select(.kind == "ClusterRole"))' $operator_yaml_path)
 }
 
 verify_no_crds_diff (){
+  echo "check crds alignment"
   source hack/get_information_helper.sh
   are_manifest_files_exsists_in_current_csi_version
   crd_files=$(get_bundle_crds)
   for crd_file in $crd_files; do
     diff $origin_crd_yaml_path $crd_file
   done
-  diff <(cat $origin_crd_yaml_path) <(yq eval '(. | select(.kind == "CustomResourceDefinition"))' $operator_yaml_path)
 }
 
 check_generation
+verify_full_operator_yaml_is_align
 verify_no_roles_diff
+verify_no_crds_diff
