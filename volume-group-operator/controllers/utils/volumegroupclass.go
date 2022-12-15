79bc54fcbf9376a2a3db72c45b1b33d5397881d0
ArbelNathan <arbel.nathan@ibm.com>
2022-12-15 11:30:49 +0200

    PR, Get instead of List
    Signed-off-by: ArbelNathan <arbel.nathan@ibm.com>

--- volume-group-operator/controllers/utils/volumegroupclass.go
+++ volume-group-operator/controllers/utils/volumegroupclass.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021.
+Copyright 2022.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,34 +18,24 @@ package utils
 
 import (
 	"context"
-	"fmt"
 	volumegroupv1 "github.com/IBM/volume-group-operator/api/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
 	"github.com/go-logr/logr"
-	"k8s.io/apimachinery/pkg/api/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // GetVolumeGroupClass get volume group class object from the subjected namespace and return the same.
 func (r *ControllerUtils) GetVolumeGroupClass(logger logr.Logger, vgcName string) (*volumegroupv1.VolumeGroupClass, error) {
-	vgcObjl := &volumegroupv1.VolumeGroupClassList{}
-	err := r.Client.List(context.TODO(), vgcObjl, &client.ListOptions{Raw: &metav1.ListOptions{FieldSelector: fmt.Sprintf("metadata.name=%s", vgcName)}})
+	vgcObj := &volumegroupv1.VolumeGroupClass{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: vgcName}, vgcObj)
 	if err != nil {
-		if errors.IsNotFound(err) {
-			logger.Error(err, "VolumeGroupClass not found", "VolumeGroupClass", vgcName)
+		if apierrors.IsNotFound(err) {
+			logger.Error(err, "VolumeGroupClass not found", "VolumeGroupClass Name", vgcName)
 		} else {
 			logger.Error(err, "Got an unexpected error while fetching VolumeGroupClass", "VolumeGroupClass", vgcName)
 		}
 
 		return nil, err
 	}
-	items := vgcObjl.Items
-	if len(items) > 1 {
-		var Ierr error
-		Ierr = fmt.Errorf("got an unexpected amount of object while fetching VolumeGroupClass %s", vgcName)
-		return nil, Ierr
-	}
-	vgcObj := &items[0]
 	return vgcObj, nil
 }
