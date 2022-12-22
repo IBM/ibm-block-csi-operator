c61dbe00c372c3e119377be33877a0d1a3f22ec8
Elena Gershkovich <elenage@il.ibm.com>
2022-12-22 14:49:52 +0200

    Task/csi 4888 wait for crds (#297)
    * Wait for volume group CRDs
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>
    
    * Wait for volume group CRDs
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>
    
    * Wait for volume group CRDs
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>
    
    * Wait for volume group CRDs
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>
    
    * Wait for volume group CRDs
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>
    
    * Wait for volume group CRDs
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>
    
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>

--- volume-group-operator/controllers/volumegroup_controller.go
+++ volume-group-operator/controllers/volumegroup_controller.go
@@ -22,14 +22,23 @@ import (
 	"github.com/IBM/volume-group-operator/pkg/config"
 	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	volumegroupv1 "github.com/IBM/volume-group-operator/api/v1"
 	grpcClient "github.com/IBM/volume-group-operator/pkg/client"
+)
+
+const (
+	VolumeGroup               = "VolumeGroup"
+	VolumeGroupClass          = "VolumeGroupClass"
+	VolumeGroupContent        = "VolumeGroupContent"
 )
 
 // VolumeGroupReconciler reconciles a VolumeGroup object
@@ -53,8 +62,6 @@ type VolumeGroupReconciler struct {
 //+kubebuilder:rbac:groups="",resources=persistentvolumeclaims/finalizers,verbs=update
 
 func (r *VolumeGroupReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
 	// TODO(user): your logic here
 
 	return ctrl.Result{}, nil
@@ -62,6 +69,13 @@ func (r *VolumeGroupReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *VolumeGroupReconciler) SetupWithManager(mgr ctrl.Manager, cfg *config.DriverConfig) error {
+	logger := r.Log.WithName("SetupWithManager")
+	err := r.waitForCrds(logger)
+	if err != nil {
+		r.Log.Error(err, "failed to wait for crds")
+
+		return err
+	}
 	pred := predicate.GenerationChangedPredicate{}
 
 	r.VolumeGroupClient = grpcClient.NewVolumeGroupClient(r.GRPCClient.Client, cfg.RPCTimeout)
@@ -69,4 +83,52 @@ func (r *VolumeGroupReconciler) SetupWithManager(mgr ctrl.Manager, cfg *config.D
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&volumegroupv1.VolumeGroup{}).
 		WithEventFilter(pred).Complete(r)
+}
+
+func (r *VolumeGroupReconciler) waitForCrds(logger logr.Logger) error {
+	err := r.waitForVolumeGroupResource(logger, VolumeGroup)
+	if err != nil {
+		logger.Error(err, "failed to wait for VolumeGroup CRD")
+
+		return err
+	}
+
+	err = r.waitForVolumeGroupResource(logger, VolumeGroupClass)
+	if err != nil {
+		logger.Error(err, "failed to wait for VolumeGroupClass CRD")
+
+		return err
+	}
+
+	err = r.waitForVolumeGroupResource(logger, VolumeGroupContent)
+	if err != nil {
+		logger.Error(err, "failed to wait for VolumeGroupContent CRD")
+
+		return err
+	}
+
+	return nil
+}
+
+func (r *VolumeGroupReconciler) waitForVolumeGroupResource(logger logr.Logger, resourceName string) error {
+	unstructuredResource := &unstructured.UnstructuredList{}
+	unstructuredResource.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   volumegroupv1.GroupVersion.Group,
+		Kind:    resourceName,
+		Version: volumegroupv1.GroupVersion.Version,
+	})
+	for {
+		err := r.Client.List(context.TODO(), unstructuredResource)
+		if err == nil {
+			return nil
+		}
+		// return errors other than NoMatch
+		if !meta.IsNoMatchError(err) {
+			logger.Error(err, "got an unexpected error while waiting for resource", "Resource", resourceName)
+
+			return err
+		}
+		logger.Info("resource does not exist", "Resource", resourceName)
+		time.Sleep(5 * time.Second)
+	}
 }
