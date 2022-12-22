2ad5e6ed70577bf6945c0a14f1b6871b7ca1ecf7
matancarmeli7 <matan.carmeli7@gmail.com>
2022-12-22 17:15:42 +0200

    ensure pvc is in bound phase
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- volume-group-operator/pkg/messages/controller_messages.go
+++ volume-group-operator/pkg/messages/controller_messages.go
@@ -33,4 +33,5 @@ var (
 	GetVolumeGroupContentOfVolumeGroup               = "Get matching volumeGroupContent from %s/%s VolumeGroup"
 	RemovePersistentVolumeFromVolumeGroupContent     = "Removing %s/%s persistentVolume from %s/%s volumeGroupContent"
 	RemovedPersistentVolumeFromVolumeGroupContent    = "Successfully removed %s/%s persistentVolume from %s/%s volumeGroupContent"
+	PersistentVolumeClaimIsNotInBoundPhase           = "PersistentVolumeClaim is not in bound phase, stopping the reconcile, when it will be in bound phase, reconcile will continue"
 )
