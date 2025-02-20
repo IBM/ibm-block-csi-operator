866fb99bdd53c0f1077dccd9490d6703cccf2d01
Ariel Kass <arielk@il.ibm.com>
2025-02-20 09:31:38 +0200

    update copyright to 2025

--- build/ci/run_olm_validation.sh
+++ build/ci/run_olm_validation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 #
-# Copyright 2020 IBM Corp.
+# Copyright 2025 IBM Corp.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
